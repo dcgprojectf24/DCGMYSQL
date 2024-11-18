8d199b6aa4846570c998542238e7aab08be55b43
BadPolarWolf <puccinibocelli@hotmail.com>
2024-11-17 19:09:16 -1000

    ...

--- public/functions.js
+++ public/functions.js
@@ -53,12 +53,6 @@ function getCurrentDate() {// Function to get the current date in the format YYY
     return `${year}-${month}-${day}`;
 }
 
-function generatePassword(){// Used in conjunction with the function above. This function is called at the click of a button and it takes the value of the generatePassword function and returns it to a specificied elementID
-  const passwordField = document.getElementById("password");
-  const generatedPassword = generateRandomPassword(10); // Change the length as needed
-  passwordField.value = generatedPassword;
-}
-
 function logout() {// deletes the logged in cookie and reloads the page
   // Deletes all cookies
   document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
@@ -95,97 +89,6 @@ function reloadPageFor1Seconds() {
           clearInterval(reloadInterval);
       }
   }, 1000); // 1000 milliseconds = 1 second
-}
-
-/*---------------------------------- PRODUCTS PAGE SPECIFIC FUNCTIONS ----------------------------------*/
-function productsTable(){// Function that generates the products on the products.html page
-    for (let i in products){
-      let a_qty = shopping_cart[prod_key][`quantity${i}`];
-      let fav = shopping_cart[prod_key][`favorite${i}`];
-        document.write(`   
-            <div class="product-item">
-                <img class="product-image" src="${products[i].image}">
-                <div class="product-title">${products[i].brand}</div>
-                <div class="product-description">${products[i].description}</div>
-                <span class="product-price">$${products[i].price}</span>
-                <lable>Favorite:</lable><input type="checkbox" id="checkbox${prod_key}_${i}" name="favorite${i}" onchange="updateFav('${prod_key}',${i});"> 
-                <div class="product-avaliability">Product Avaliability: ${products[i].aval}</div>
-                <label id="quantity${i}_label"}">Quantity:</label>
-                <div style="color: blue;" id="active_error${i}"></div>
-                <input type="number" id="textbox${prod_key}_${i}" name="quantity${i}" onchange="updateQuantity('${prod_key}',${i});" value="${a_qty}" min="0">
-                <span id="quantity${i}_cart_label"}">In cart: ${a_qty}</span> 
-               <div style="color: red;" id="error_message${i}"></div>
-            </div>
-        `); 
-        if(fav == "on"){
-          document.getElementById(`checkbox${prod_key}_${i}`).checked = true;
-        } else {
-          document.getElementById(`checkbox${prod_key}_${i}`).checked = false;
-        }  
-    };
-} 
-
-function productsPageErrors(){// Interprets the errors from the query string and displays them
-    // the following only occurs when the page is loaded (as opposed to the function after this which executes 10 times a sec)
-    window.onload=function(){
-    // intepretting the information given to us by the server
-    let params = (new URL(document.location)).searchParams;
-    // if the query string has Input, print the error and change the button
-    if ((params.get(`error`)) === `NoInput`) { 
-      document.getElementById(`leButton`).innerHTML = "No input: select some items to purchase";
-    } else {
-     for (let i in products) {
-      // if the query string has an error values in it, print the error under the input box and then change the button accordingly
-      if (params.has(`error_quantity${i}`)) {
-        document.getElementById(`error_message${i}`).innerHTML = params.get(`error_quantity${i}`);
-        if (params.get(`error_quantity${i}`)===`Not a number!`) { 
-        document.getElementById(`leButton`).innerHTML = "Not a number: input a number";
-        } else if (params.get(`error_quantity${i}`)===`Negative value!`) { 
-        document.getElementById(`leButton`).innerHTML = "Negative value: input a positive value";
-        } else if (params.get(`error_quantity${i}`)===`Not an integer!`) { 
-        document.getElementById(`leButton`).innerHTML = "Not an integer: input a whole number";
-        } else { 
-        document.getElementById(`leButton`).innerHTML = "Unavaliable quantity";
-        };
-      };
-     }; 
-    }; 
-    };
-  // calls the checkTextBox function every 1/10 of a second (found in functions)
-  // discovered this existed in October from W3 schools ( https://www.w3schools.com/jsref/met_win_setinterval.asp )
-  setInterval(checkTextBox, 100);
-}
-
-function loginError(){ //shows the login errors
-  let params = (new URL(document.location)).searchParams; //gets the query string for errors
-  console.log(`yes`);
-  if (params.get(`error`)=== `user`) {
-    console.log(`yes1`);
-    document.getElementById(`error_message`).innerHTML = 'User does not exist!';
-    };
-  if (params.get(`error`)=== `pass`) { 
-    console.log(`yes2`);
-    document.getElementById(`error_message`).innerHTML = "Invalid Password";
-    };
-}
-
-function checkTextBox(){// Used in products.html to display the active errors in the textboxes
-    for(let i in products){// the loop is necessary so that all textboxes dont show the same thing
-      // getting the value from the textbox
-      let textBoxValue = document.getElementById(`textbox${prod_key}_${i}`).value;
-      // if the textboxvalue is 0 or nothing, return empty '', else if return whatever the error is, else return a "you want--"
-      if(textBoxValue === '' || textBoxValue == 0 || textBoxValue == null){
-        document.getElementById(`active_error${i}`).innerHTML = "";
-      } else if (Number(textBoxValue) != textBoxValue){
-        document.getElementById(`active_error${i}`).innerHTML = "Active Error: Not a number!";
-      } else if (textBoxValue < 0) {
-        document.getElementById(`active_error${i}`).innerHTML = "Active Error: Negative value!";
-      } else if (parseInt(textBoxValue) != textBoxValue) {
-        document.getElementById(`active_error${i}`).innerHTML = "Active Error: Not an integer!";
-      } else if (Number(textBoxValue) > 0) {
-        document.getElementById(`active_error${i}`).innerHTML = `You want: ${textBoxValue}`;
-      }
-    };
 }
 
 /*------------------------- INVOICE AND SHOPPING CART PAGE SPECIFIC FUNCTIONS --------------------------*/
