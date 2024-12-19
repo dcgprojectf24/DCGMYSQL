5242c901e7b1cf83b5f150fe2a57cd0b436992ef
BadPolarWolf <puccinibocelli@hotmail.com>
2024-12-18 23:05:16 -1000

    ...

--- old PHP files/example.php
+++
@@ -1,8 +0,0 @@
-<! HTML form to get query parameters from a user >
-<Form Action="next.php" method="POST">
-<H1>Welcome to the Room Browsing Program</H1>
-Enter minimum room price <input type = 'text' name = 'Min_Price'><BR>
-Enter maximum room price <input type = 'text' name = 'Max_Price'><BR>
-<input type="submit" value="Submit">
-</Form>
-
